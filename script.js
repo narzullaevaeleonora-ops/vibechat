49f7e1b4e041bb32252ce1bcbc7889427ede7d3b
narzullaevaeleonora-ops <narzullaevaeleonora@gmail.com>
2025-09-18 22:58:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-const form = document.getElementById('chat-form');
-const input = document.getElementById('user-input');
-const messages = document.getElementById('messages');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const text = input.value.trim();
-  if (!text) return;
-  addMessage(text, 'user');
-  input.value = '';
-  // Имитация ответа
-  setTimeout(() => addMessage('Это автоответчик вайб-чата 😎', 'bot'), 800);
-});
-
-function addMessage(text, sender) {
-  const div = document.createElement('div');
-  div.className = 'message ' + sender;
-  div.textContent = text;
-  messages.appendChild(div);
-  messages.scrollTop = messages.scrollHeight;
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGopVUpkWCIw6u9oMC-RTabGbg7PgPVSw",
+  authDomain: "vibechat-f931b.firebaseapp.com",
+  projectId: "vibechat-f931b",
+  storageBucket: "vibechat-f931b.firebasestorage.app",
+  messagingSenderId: "819501932717",
+  appId: "1:819501932717:web:7d4470f7e0fbf36c68a624",
+  measurementId: "G-2679944J8M"
+};
